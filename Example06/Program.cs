6b13a5698669f34cec3861fb36b20e06559a6646
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2025-07-13 14:23:50 +0300

    feat<Example06>: Выполнить задание по веб-серверу

---
+++ Example06/Program.cs
@@ -0,0 +1,41 @@
+﻿bool running = true;
+Queue<string?> requests = new();
+
+Thread monitorThread = new(MonitorQueue);
+monitorThread.Start();
+Console.WriteLine("Веб-сервер запущен: ");
+
+while (running)
+{
+    Console.Write("Запрос: ");
+    string? request = Console.ReadLine();
+    if (request?.ToLower() == "q")
+    {
+        running = false;
+        break;
+    }
+    requests.Enqueue(request);
+}
+
+monitorThread.Join();
+
+void MonitorQueue()
+{
+    while (running)
+    {
+        if (requests.TryDequeue(out var request) && request is not null)
+        {
+            new Thread(() => ProcessRequest(request)).Start();
+        }
+        else
+        {
+            Thread.Sleep(50);
+        }
+    }
+}
+
+void ProcessRequest(string? request)
+{
+    Thread.Sleep(3000); // Имитация времени обработки запроса
+    Console.WriteLine($"\nОбработан запрос: '{request}'...");
+}
