4bcea5827d1847d2c1e468e4a74d8bdaa79ecb1d
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2025-07-13 13:37:56 +0300

    feat: Example03, Example04, Example05

---
+++ Example03/Program.cs
@@ -0,0 +1,31 @@
+﻿void PrintThreadIds()
+{
+    int remainingIterations = 100;
+    while (remainingIterations-- > 0)
+    {
+        Thread currentThread = Thread.CurrentThread;
+        Console.Write(currentThread.Name + " ");
+        Thread.Sleep(10);
+    }
+}
+
+int count = 10;
+
+Thread[] threads = new Thread[count];
+
+for (int i = 0; i < count; i++)
+{
+    Thread thread = threads[i];
+    thread = new Thread(PrintThreadIds);
+    if (i < 5)
+    {
+        thread.Priority = ThreadPriority.Lowest;
+        thread.Name = "L";
+    }
+    else
+    {
+        thread.Priority = ThreadPriority.Highest;
+        thread.Name = "H";
+    }
+    thread.Start();
+}
---
+++ Example04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+const int n = 1_000_000_000;
+
+int[] numbers = Enumerable.Range(1, n).ToArray();
+
+decimal totalSum = 0;
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+for (int i = 0; i < n; i++)
+{
+    totalSum += numbers[i];
+}
+
+sw.Stop();
+
+Console.WriteLine($"ElapsedMilliseconds: {sw.ElapsedMilliseconds}");
+Console.WriteLine($"Total sum: {totalSum:F0}");
+
+// ===
+
+sw.Reset();
+sw.Start();
+totalSum = 0;
+var chunks = numbers.Chunk(n / 5).ToArray();
+var threads = new Thread[5];
+for (int i = 0; i < threads.Length; i++)
+{
+    var chunkNum = i;
+    threads[i] = new Thread(() =>
+    {
+        for(int b = 0; b < chunks[chunkNum].Length; b++)
+        {
+            totalSum += chunks[chunkNum][b];
+        }
+    });
+}
+
+foreach (var thread in threads)
+{
+    thread.Start();
+    thread.Join();
+}
+
+sw.Stop();
+Console.WriteLine($"ElapsedMilliseconds: {sw.ElapsedMilliseconds}");
+Console.WriteLine($"Total sum: {totalSum:F0}");
