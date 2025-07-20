6a4199d424550d3e993ce6690daf7dda1659e3e2
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2025-07-20 09:10:51 +0300

    feat<Example16>: Демонстрация возвращения результата из вторичного потока

---
+++ Example16/Program.cs
@@ -0,0 +1,17 @@
+﻿int result = 0;
+
+Thread worker = new Thread(() =>
+{
+   result = ComputeValue(10, 20);
+});
+
+worker.Start();
+//worker.Join();
+
+Console.WriteLine($"Результат: {result}");
+
+int ComputeValue(int a, int b)
+{
+   Thread.Sleep(2000);
+   return a + b;
+}
